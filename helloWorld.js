e06592a8913d7989075584528ae6ab9a639d06c8
Artem Danshin <a.danshin@rambler-co.ru>
2016-12-28 23:48:11 +0300

    Easy route

--- helloWorld.js
+++ helloWorld.js
@@ -1,8 +1,21 @@
 var http = require('http');
 
 http.createServer(function(req,res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.end('Hello world!');
+  var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch(path) {
+    case '':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Homepage');
+      break;
+    case '/about':
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('0');
+      break;
+    default:
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('Не найдено');
+      break;
+  }
 }).listen(3000);
 
 console.log('Сервер запущен на localhost:3000; нажмите Ctrl+C для завершения...')
