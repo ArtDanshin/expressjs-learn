caf2b74843ed1e02bfaeb2786009f50056ef5149
Artem Danshin <a.danshin@rambler-co.ru>
2016-12-31 20:57:53 +0300

    Add template builder Handlebars. HTML files converted to .hbs. Configure public folder

--- meadowlark.js
+++ meadowlark.js
@@ -1,30 +1,36 @@
 var express = require('express');
+var exphbs = require('express-handlebars');
 
 var app = express();
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: '.hbs'
+});
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Meadowlark Travel')
+  res.render('home');
 })
 
 app.get('/about', function(req, res){
-  res.type('text/plain');
-  res.send('О Meadowlark Travel');
+  res.render('about');
 })
 
 app.use(function(req, res){
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Не найдено');
+  res.render('404');
 })
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
-  res.type('text/plain');
   res.status(500);
-  res.send('500 - Ошибка сервера');
+  res.render('500');
 })
 
 app.listen(app.get('port'), function(){
