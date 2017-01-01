cbeb5511ae1b7361adae561ca3e328637b1913b6
Artem Danshin <a.danshin@rambler-co.ru>
2017-01-01 14:05:56 +0300

    Tours page added

--- meadowlark.js
+++ meadowlark.js
@@ -22,26 +22,34 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('home');
-})
+});
 
 app.get('/about', function(req, res){
   res.render('about', {
     fortune: fortune.getFortune(),
     pageTestScript: '/qa/tests-about.js'
   });
-})
+});
+
+app.get('/tours/hood-river', function(req, res){
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', function(req, res){
+  res.render('tours/request-group-rate');
+});
 
 app.use(function(req, res){
   res.status(404);
   res.render('404');
-})
+});
 
 app.use(function(err, req, res, next){
   console.error(err.stack);
   res.status(500);
   res.render('500');
-})
+});
 
 app.listen(app.get('port'), function(){
   console.log('Express запущен на http://localhost:' + app.get('port') + ' ; нажмите Ctrl + C для завершения');
-})
+});
