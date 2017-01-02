cc5b38dd20fcde3f0dbbfdacc11ca071109eb176
Artem Danshin <a.danshin@rambler-co.ru>
2017-01-02 18:08:08 +0300

    Processing original form added. Body-parser added

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
 var fortune = require('./lib/fortune.js');
 var weather = require('./lib/weather.js');
 
@@ -30,12 +31,26 @@ app.use(function(req, res, next){
   if (!res.locals.partials) res.locals.partials = {};
   res.locals.partials.weatherContext = weather.getWeatherData();
   next();
-})
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.get('/newsletter', function(req, res){
+  res.render('newsletter', { csrf: 'CSRF token goes here' });
+})
+
+app.post('/process', function(req, res){
+  console.log('Form (from querystring): ' + req.query.form);
+  console.log('CSRF token (from hidden form field): ' + req.body._csrf);
+  console.log('Name (from visible form field): ' + req.body.name);
+  console.log('Email (from visible form field): ' + req.body.email);
+  res.redirect(303, '/thank-you' );
 });
 
 app.get('/about', function(req, res){
