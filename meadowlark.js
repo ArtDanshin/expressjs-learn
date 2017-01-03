51d16a1fee551b7000d75b90ace7bbbe51986a39
Artem Danshin <a.danshin@rambler-co.ru>
2017-01-04 01:52:38 +0300

    MongoDB connected and runned. FIles archived in data folder

--- meadowlark.js
+++ meadowlark.js
@@ -5,9 +5,13 @@ var formidable = require('formidable');
 var cookieParser = require('cookie-parser');
 var morgan = require('morgan');
 var explogger = require('express-logger');
+var expsession = require('express-session');
+var fs = require('fs');
+var mongoose = require('mongoose');
 var fortune = require('./lib/fortune.js');
 var weather = require('./lib/weather.js');
 var credentials = require('./config/credentials.js');
+var Vacation = require('./models/vacations.js');
 
 var app = express();
 var hbs = exphbs.create({
@@ -25,18 +29,81 @@ var hbs = exphbs.create({
 app.engine('.hbs', hbs.engine);
 app.set('view engine', '.hbs');
 
+var opts = {
+  server: {
+    socketOptions: { keepAlive: 1 }
+  }
+};
+
 app.set('port', process.env.PORT || 3000);
 
 switch (app.get('env')){
   case 'development':
+    mongoose.connect(credentials.mongo.development.connectionString, opts);
     app.use(morgan('dev'));
     break;
   case 'production':
+    mongoose.connect(credentials.mongo.production.connectionString, opts);
     app.use(explogger({
       path: __dirname + '/log/requests.log'
     }))
     break;
 }
+
+Vacation.find(function(err, vacations){
+  if (err) return console.error(err);
+
+  if (vacations.length) return;
+
+  new Vacation({
+    name: 'Однодневный тур по реке Худ',
+    slug: 'hood-river-day-trip',
+    category: 'Однодневный тур',
+    sku: 'HR199',
+    description: 'Проведите день в плавании по реке Колумбия ' +
+    'и насладитесь сваренным по традиционным рецептам ' +
+      'пивом на реке Худ!',
+    priceInCents: 9995,
+    tags: ['однодневный тур', 'река худ', 'плавание', 'виндсерфинг', 'пивоварни'],
+    inSeason: true,
+    maximumGuests: 16,
+    available: true,
+    packagesSold: 0
+  }).save();
+
+  new Vacation({
+    name: 'Отдых в Орегон Коуст',
+    slug: 'oregon-coast-getaway',
+    category: 'Отдых на выходных',
+    sku: 'OC39',
+    description: 'Насладитесь океанским воздухом ' +
+    'и причудливыми прибрежными городками!',
+    priceInCents: 269995,
+    tags: ['отдых на выходных', 'орегон коуст',
+      'прогулки по пляжу'],
+    inSeason: false,
+    maximumGuests: 8,
+    available: true,
+    packagesSold: 0,
+  }).save();
+  new Vacation({
+    name: 'Скалолазание в Бенде',
+    slug: 'rock-climbing-in-bend',
+    category: 'Приключение',
+    sku: 'B99',
+    description: 'Пощекочите себе нервы горным восхождением ' +
+    'на пустынной возвышенности.',
+    priceInCents: 289995,
+    tags: ['отдых на выходных', 'бенд', 'пустынная возвышенность', 'скалолазание'],
+    inSeason: true,
+    requiresWaiver: true,
+    maximumGuests: 4,
+    available: false,
+    packagesSold: 0,
+    notes: 'Гид по данному туру в настоящий момент ' +
+    'восстанавливается после лыжной травмы.',
+  }).save();
+});
 
 app.use(function(req, res, next) {
   res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
@@ -52,6 +119,11 @@ app.use(function(req, res, next){
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser(credentials.cookieSecret));
+app.use(expsession({
+  resave: false,
+  saveUninitialized: false,
+  secret: credentials.cookieSecret
+}));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -67,19 +139,61 @@ app.get('/contest/vacation-photo', function(req, res){
   })
 });
 
+var dataDir = __dirname + '/data';
+var vacationPhotoDir = dataDir + '/vacation-photo';
+fs.existsSync(dataDir) || fs.mkdirSync(dataDir);
+fs.existsSync(vacationPhotoDir) || fs.mkdirSync(vacationPhotoDir);
+
+function saveContestEntry(contestName, email, year, month, photoPath){
+  //TODO... Это будет добавленно позднее
+}
+
 app.post('/contest/vacation-photo/:year/:month', function(req, res){
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files){
-    if (err) return res.redirect(303, '/error');
-    console.log('received fields: ' + fields);
-    console.log('received files: ' + files);
-    res.redirect(303, '/thank-you');
+    if (err) {
+      res.session.flash = {
+        type: 'danger',
+        intro: 'Упс!',
+        message: 'Во время обработки отправленной Вами формы произошла ошибка. Пожалуйста попробуйте еще раз.'
+      };
+      return res.redirect(303, '/contest/vacation-photo');
+    }
+    var photo = files.photo;
+    var dir = vacationPhotoDir + '/' + photo.name;
+    var path = dir + '/' + photo.name;
+    fs.mkdirSync(dir);
+    fs.renameSync(photo.path, dir + '/' + photo.name);
+    saveContestEntry('vacation-photo', fields.email, req.params.years, req.params.nonth, path);
+    req.session.flash = {
+      type: 'success',
+      intro: 'Удачи!',
+      message: 'Вы стали участником конкурса'
+    }
+    res.redirect(303, '/contest/vacation-photo/entries');
+  })
+});
+
+app.get('/vacations', function(req, res){
+  Vacation.find({ available: true }, function(err, vacations){
+    var context = {
+      vacations: vacations.map(function(vacation){
+        return {
+          vacations: vacations.sku,
+          name: vacation.name,
+          description: vacation.description,
+          price: vacation.getDisplayPrice(),
+          inSeason: vacation.inSeason
+        }
+      })
+    };
+    res.render('vacations', context);
   })
 })
 
 app.get('/newsletter', function(req, res){
   res.render('newsletter', { csrf: 'CSRF token goes here' });
-})
+});
 
 app.post('/process', function(req, res){
   if (req.xhr || req.accepts('json.html') === 'json') {
