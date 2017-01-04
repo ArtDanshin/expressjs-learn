316349b94b3d17f735086ede8bb05b374654fbc8
Artem Danshin <a.danshin@rambler-co.ru>
2017-01-04 14:21:12 +0300

    Delete credential?!

---
+++ config/credentials.js
@@ -0,0 +1,11 @@
+module.exports = {
+  cookieSecret: 'his fruit should income',
+  mongo: {
+    development: {
+      connectionString: 'mongodb://localhost/learn'
+    },
+    production: {
+      connectionString: 'mongodb://localhost/learn'
+    }
+  }
+};
